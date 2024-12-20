31fc72c3e1559124ae563d74ff4924d41ff84e27
DIVESH PATIL <diveshpatil9104@gmail.com>
2024-12-20 20:33:15 +0530

    project

---
+++ connection.py
@@ -0,0 +1,17 @@
+import mysql.connector
+try:
+    con = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Raj@9104",
+        database="pharmacy",
+        port=3306
+        # UseSSL=false
+    )
+    cursor=con.cursor()
+    cursor.execute("SELECT * FROM appuser WHERE userRole='Admin'")
+    res=cursor.fetchall()
+    print(res)
+    print("Connection Successful!!")
+except mysql.connector.Error as e:
+    print(e)
