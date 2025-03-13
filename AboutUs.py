1e3ffdaf95bbbbe2d46cda5a08d07a4b7fede136
DIVESH PATIL <diveshpatil9104@gmail.com>
2025-03-13 23:39:18 +0530

    project

--- AboutUs.py
+++ AboutUs.py
@@ -1,6 +1,6 @@
 import              tkinter as tk
 from                  tkinter import ttk
-from PIL import Image, ImageTk, ImageDraw
+from PIL import   Image, ImageTk, ImageDraw
 
 class AboutUsPage(tk.Toplevel):
     def __init__(self):
